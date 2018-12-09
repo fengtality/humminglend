7678f540c5c89b3429ea5b6c230eed73444813c7
Michael Feng <mifeng@gmail.com>
2018-12-09 09:07:11 +0800

    money market rate, supply API in progress

--- balances.js
+++ balances.js
@@ -30,14 +30,33 @@ const getTokenBalancePromise = (symbol) => new Promise((resolve, reject) => {
 
     tokenContract.methods.balanceOf(walletAddress).call((err, bal) => {
         if (err) reject({ error: 'Token balance not available', timestamp: new Date() })
-        // else resolve({ balance: web3.fromWei(bal, 'ether').toNumber(), timestamp: new Date() })
-
         else {
             tokenContract.methods.allowance(walletAddress, moneyMarketAddress).call((err, allow) => {
                 if (err) reject({ error: 'Allowance not available', timestamp: new Date() })
+                const allowBool = web3.utils.fromWei(allow, 'ether') >= 10000000000000000000000 ? true : false
                 resolve({
                     balance: web3.utils.fromWei(bal, 'ether'), 
-                    allowance: web3.utils.fromWei(allow, 'ether'), 
+                    allowance: allowBool,
+                    timestamp: new Date() 
+                })
+            })
+        }
+    })
+})
+
+const convertRateToAnnualPct = (rate) => Math.round(web3.utils.fromWei(rate, 'ether') * 3600 / 15 * 24 * 365 * 10000) / 100
+
+const getMoneyMarketPromise = (symbol) => new Promise((resolve, reject) => {
+    const tokenAddress = tokenAddresses[symbol]
+    const moneyMarketContract = new web3.eth.Contract(moneyMarketInterface, moneyMarketAddress)
+    moneyMarketContract.methods.markets(tokenAddress).call((err, market) => {
+        if (err) reject({ error: 'Market rate not available', timestamp: new Date() })
+        else {
+            moneyMarketContract.methods.getSupplyBalance(walletAddress,tokenAddress).call((err, supplyBal) => {
+                if (err) reject({ error: 'Supply balance not available', timestamp: new Date() })
+                resolve({ 
+                    supplyRate: convertRateToAnnualPct(market.supplyRateMantissa),
+                    supplyBalance: web3.utils.fromWei(supplyBal, 'ether'),
                     timestamp: new Date() 
                 })
             })
@@ -46,7 +65,6 @@ const getTokenBalancePromise = (symbol) => new Promise((resolve, reject) => {
 })
 
 const approveToken = (symbol) => {
-    console.log(symbol)
     const tokenAddress = tokenAddresses[symbol]
     const tokenContract = new web3.eth.Contract(tokenInterface, tokenAddress)
 
@@ -55,8 +73,21 @@ const approveToken = (symbol) => {
     }).then(console.log)
 }
 
+const supplyToken = (symbol) => {
+    const tokenAddress = tokenAddresses[symbol]
+    const moneyMarketContract = new web3.eth.Contract(moneyMarketInterface, moneyMarketAddress)
+    const amount = 100000
+
+    moneyMarketContract.methods.supply(tokenAddress, amount).send({
+        from: walletAddress
+    }).then(console.log)
+}
+
 module.exports = {
+    walletAddress,
     getWalletBalancePromise,
     getTokenBalancePromise,
+    getMoneyMarketPromise,
     approveToken,
+    supplyToken,
 }
--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const axios = require('axios');
 const path = require('path')
 
-const { getWalletBalancePromise, 
+const { walletAddress,
+        getWalletBalancePromise, 
         getTokenBalancePromise,
+        getMoneyMarketPromise,
         approveToken,
+        supplyToken,
 } = require('./balances')
 
 const {
@@ -17,14 +20,18 @@ const {
 // Express
 const app = express()
 app.get('/', (req, res) => { 
-    res.sendFile(path.join(__dirname + "/views/index.html"))
+    res.sendFile(path.join(__dirname + "/index.html"))
 })
 
-app.post('/approve/:symbol', (req, res) => { 
+app.get('/approve/:symbol', (req, res) => { 
     const symbol = req.params.symbol
     approveToken(symbol)
 })
 
+app.get('/supply/:symbol', (req, res) => { 
+    const symbol = req.params.symbol
+    supplyToken(symbol)
+})
 
 const PORT = 3000
 app.listen(PORT, () => {
@@ -37,17 +44,40 @@ app.listen(PORT, () => {
 
 // Resolve promises
 let promises = [getWalletBalancePromise()];
+
+// Token balances
 promises = promises.concat(getTokenBalancePromise("bat"));
 promises = promises.concat(getTokenBalancePromise("dai"));
 promises = promises.concat(getTokenBalancePromise("rep"));
 promises = promises.concat(getTokenBalancePromise("weth"));
 promises = promises.concat(getTokenBalancePromise("zrx"));
 
+// Lending rates
+promises = promises.concat(getMoneyMarketPromise("bat"));
+promises = promises.concat(getMoneyMarketPromise("dai"));
+promises = promises.concat(getMoneyMarketPromise("rep"));
+promises = promises.concat(getMoneyMarketPromise("weth"));
+promises = promises.concat(getMoneyMarketPromise("zrx"));
+
 return Promise.all(promises).then((values) => {
-    console.log('ETH | balance: ',  values[0].balance)
-    console.log('BAT | balance: ',  values[1].balance, `| allowance: `, values[1].allowance)
-    console.log('DAI | balance: ',  values[2].balance, `| allowance: `, values[2].allowance)
-    console.log('REP | balance: ',  values[3].balance, `| allowance: `, values[3].allowance)
-    console.log('WETH | balance: ', values[4].balance, `| allowance: `, values[4].allowance)
-    console.log('ZRX | balance: ',  values[5].balance, `| allowance: `, values[5].allowance)
+    console.log('Wallet address:', walletAddress)
+    console.log('Wallet ETH balance:', values[0].balance)
+    console.log('')
+    console.log('Token balances:')
+    console.log('Token | Balance |  Allowance')
+    console.log('------------------------------')
+    console.log('BAT   | ', values[1].balance, `   | `, values[1].allowance)
+    console.log('DAI   | ', values[2].balance, `   | `, values[2].allowance)
+    console.log('REP   | ', values[3].balance, `   | `, values[3].allowance)
+    console.log('WETH  | ', values[4].balance, `    | `, values[4].allowance)
+    console.log('ZRX   | ', values[5].balance, `   | `, values[5].allowance)
+    console.log('')
+    console.log('Lending rates:')
+    console.log('Token |  Rate   |  Supplied')
+    console.log('------------------------------')
+    console.log('BAT   | ', values[6].supplyRate, `  | `, values[6].supplyBalance)
+    console.log('DAI   | ', values[7].supplyRate, `  | `, values[7].supplyBalance)
+    console.log('REP   | ', values[8].supplyRate, `  | `, values[8].supplyBalance)
+    console.log('WETH  | ', values[9].supplyRate, `  | `, values[9].supplyBalance)
+    console.log('ZRX   | ', values[10].supplyRate, `  | `, values[10].supplyBalance)
 })
